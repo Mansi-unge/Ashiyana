114229643384657aa4cdb392418e4f98465fde3c
Mansi-unge <mansiunge@gmail.com>
2025-01-19 12:12:24 +0530

    commit changes again

--- client/src/skeletons/ResidencySkelton.jsx
+++ client/src/skeletons/ResidencySkelton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ResidencySkelton = () => {
   return (
-    <div className="w-72 h-80 bg-gray-300 animate-pulse rounded-md shadow-md">
+    <div className="w-72 h-80 bg-gray-300 animate-pulse rounded-md shadow-md justify-evenly">
       <div className="w-full h-48 bg-gray-400 rounded-t-md"></div>
       <div className="p-4">
         <div className="h-6 bg-gray-400 rounded w-3/4 mb-2"></div>
