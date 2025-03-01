cdaf5ffd477cb1286bd901a972a4f46cf65fabfb
Mansi-unge <mansiunge@gmail.com>
2025-03-01 08:32:37 +0530

    add

--- client/src/skeletons/ResidencySkelton.jsx
+++ client/src/skeletons/ResidencySkelton.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const ResidencySkelton = () => {
   return (
-    <div className="w-72 h-80 bg-gray-300 animate-pulse rounded-md shadow-md justify-evenly">
-      <div className="w-full h-48 bg-gray-400 rounded-t-md"></div>
-      <div className="p-4">
-        <div className="h-6 bg-gray-400 rounded w-3/4 mb-2"></div>
-        <div className="h-4 bg-gray-400 rounded w-1/2"></div>
-      </div>
-    </div>
+    <div className="w-[300px] h-[350px] bg-gray-200 animate-pulse rounded-lg shadow-md p-4">
+    <div className="w-full h-[180px] bg-gray-300 rounded-md"></div>
+    <div className="mt-4 h-5 w-3/4 bg-gray-300 rounded"></div>
+    <div className="mt-2 h-4 w-1/2 bg-gray-300 rounded"></div>
+    <div className="mt-4 h-8 w-full bg-gray-300 rounded"></div>
+  </div>
   );
 };
 
